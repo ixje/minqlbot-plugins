ce418166cbde57edbeb33e30a467931bf4b41063
Erik van den Brink <brinkie83@gmail.com>
2015-05-14 23:05:28 +0200

    updated readme and to latest unfinished leveller code

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 minqlbot-plugins
 ================
 
-A collection of plugins for minqlbot.
+All credits go to the original author Mino. Without his work on [minqlbot](https://github.com/MinoMino/minqlbot) there would be no place for plugins.
 
-This is a repository of the default config file, the database tables (including an initialized empty database),
-and more importantly the plugins themselves.
+For the latest version of the original plugins including bux fixes visit the [original plugins repository](https://github.com/MinoMino/minqlbot-plugins)
 
-Documentation will come later.
+What new in this branch
+=======================
+
+**leveller.py** - a standalone plugin that auto levels out teams by spectating the last joiner and/or lowest score on the oversized team
+
+### Status
+Broken. Not functional in it's current state.
+
+### TODO
+* Make a more intelligent ruling system by tracking team join time and score
+* Slap Thaya to finish it
+* remove work around for game_start/round_start bug. Should be fixed in the mainbot by now.
 
 Installation
 ============
+Copy `leveller.py` to your plugins directory and add *leveller* to the plugins list in your `config.cfg`. 
 
-Look at the [main repository](https://github.com/MinoMino/minqlbot) for instructions on how to use the bot.
+For further instructions please visit the orignal author's [main repository](https://github.com/MinoMino/minqlbot). I'm usally also present in the irc channel mentioned there.
--- plugins/leveller.py
+++ plugins/leveller.py
@@ -9,34 +9,109 @@ class leveller(minqlbot.Plugin):
             self.debug("Too old version of Minqlbot. Please update to the latest version")
             minqlbot.unload_plugin("leveller")
         else:
+            self.add_hook("game_start", self.handle_game_start)
+            self.add_hook("game_end", self.handle_game_start)
             self.add_hook("round_start", self.handle_round_start)
             self.add_hook("scores", self.handle_scores)
+            self.add_hook("team_switch", self.handle_team_switch)
+            
             self.roundstart_triggered = False
+            self.blue_joins = []
+            self.red_joins = []
+            self.game_started = False
+            self.round_number = 1
+   
+
+    def handle_game_start(self, game):
+        self.debug("game_started")
+        self.game_started = True
+        self.round_number = 1
+
+    def handle_game_end(self,game,score,winner):
+        #is this triggered on an 'abort'?
+        self.game_started = False
+        self.round_number = 1
+
+    def handle_round_start(self, round):
+        self.debug("round: {}".format(round))
+        self.round_number = round
+
+        if not self.game_started:
+            self.game_started = True
+        elif self.game_started:
+            self.roundstart_triggered = True
+            self.send_command("score")
 
     def handle_scores(self, scores):
-         if self.roundstart_triggered:
+        if self.roundstart_triggered:
+            self.roundstart_triggered = False
             teams = self.teams()
-            if (len(teams["red"]) + len(teams["blue"])) % 2 == 0:
-                return
+            #if (len(teams["red"]) + len(teams["blue"])) % 2 == 0:
+            #    return
 
             if len(teams["red"]) > len(teams["blue"]):
                 reduceteam = teams["red"]
+                reduceJoinersFrom = self.red_joins
             else:
                 reduceteam = teams["blue"]
+                reduceJoinersFrom = self.blue_joins
+            #list of player names in the oversized team
             playernames = list(map(lambda p:p.clean_name, reduceteam))
-            
-            lowestscore = 999
-            toSpec = None
-            for p in scores:
-                cleanName = re.sub(r"\^[0-9]", "", str(p.player))
 
-                if cleanName in playernames:
-                    if p.score < lowestscore:
-                        toSpec = p.player
+            if self.game_started:
+                #Scenario 1, first round (gamestart, all scores 0)
+                if self.round_number == 1:
+                    self.debug("scenario 1")
+                    #always remove last joiner
+                    self.put(reduceJoinersFrom.pop(), "spectator")
+                    return
 
-            self.put(toSpec, "spectator")
-            self.roundstart_triggered = False
+                #Scenario 2, round 2 or later (on-going game)
+                if self.round_number > 1:
+                    self.debug("scenario 2")
+                    
+                    #2A - 1 joiner, spec him
+                    #2c - 2 join, 1 other leaves. spec last joiner on other team regardless of score
+                    #TODO: determine when it's scenario 2A/3C
+                    # if (scenario 2A | 2C):
+                        self.put(reduceJoinersFrom.pop(), "spectator")
+                        return
 
-    def handle_round_start(self, round):
-        self.roundstart_triggered = True
-        self.send_command("score")
+                    #2B - 1 leaver, spec lowest score on other team
+                    #TODO: determine when it's scenario 2B
+                    # if (scenario 2b):
+                        lowestscore = 999
+                        toSpec = []
+                        for p in scores:
+                            cleanName = re.sub(r"\^[0-9]", "", str(p.player))
+
+                            if cleanName in playernames:
+                                if p.score < lowestscore:
+                                    toSpec.append(p.player)
+                                if p.score == lowestscore:
+                                    toSpec.append(p.player)
+                        
+                        if len(toSpec) == 1:
+                            self.put(toSpec[0], "spectator")
+                        elif len(toSpec) >1: #2 or more with the same score. Find the last joiner
+                            
+                            #starting from the back, assuming the last added is last joined to the team
+                            for p in reversed(reduceJoinersFrom):
+                                if p in toSpec:
+                                    reduceJoinersFrom.remove(p)
+                                    self.put(p, "spectator")
+
+    def handle_team_switch(self, player, old_team, new_team):
+        #TODO: make sure the blue_joins & red_joins are updated when the bot balances without the players being considered as the last joiner
+        #      Currently the last player on the list is treated as the last joiner
+        
+        #add to a list such that we can pop the last joiner when needed
+        if old_team == "spectator" and new_team == "blue":
+            self.blue_joins.append(player)
+        if old_team == "spectator" and new_team == "red":
+            self.red_joins.append(player)
+
+        if old_team == "blue" and new_team == "spectator":
+            self.blue_joins.remove(player)
+        if old_team == "red" and new_team == "spectator":
+            self.red_joins.remove(player)
